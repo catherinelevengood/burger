b1c433b179111b0976283876584a4c0f1c727f17
Catherine Levengood <angel_avelanes@yahoo.com>
2018-11-03 20:43:29 -0400

    create folder and files ,install npm

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('1_Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('2_Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('3_Burger', false);
